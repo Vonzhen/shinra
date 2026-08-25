8189f0817ebee8ba0af190bf0795fe900905e0ac
miozen <halosvon@gmail.com>
2026-08-25 11:54:35 +0800

    fix: start ubusd before APK post-install

--- tests/openwrt-apk-inside.sh
+++ tests/openwrt-apk-inside.sh
@@ -5,6 +5,8 @@ set -eux
 APK_PACKAGE="${1:?missing APK package path}"
 
 mkdir -p /var/lock /var/run/ubus
+/sbin/ubusd &
+UBUSD_PID=$!
 apk update >/dev/null
 apk add ca-bundle >/dev/null
 apk add --allow-untrusted "$APK_PACKAGE" >/dev/null
@@ -14,8 +16,6 @@ test -f /etc/shinra/dashboard.json
 test -f /usr/share/rpcd/ucode/shinra.uc
 test -f /www/luci-static/resources/view/shinra/panel.js
 
-/sbin/ubusd &
-UBUSD_PID=$!
 /sbin/rpcd &
 RPCD_PID=$!
 
